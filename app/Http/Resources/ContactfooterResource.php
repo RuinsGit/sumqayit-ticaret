7f353d01100d5304c997eb20af5f4f70555e308d
RuinsGit <museyibli.ruhin@gmail.com>
2025-01-23 03:32:01 -0800

    des

--- app/Http/Resources/ContactfooterResource.php
+++ app/Http/Resources/ContactfooterResource.php
@@ -13,19 +13,16 @@ class ContactfooterResource extends JsonResource
             [
                 'value' => $this->number,
                 'image' => $this->number_image ? asset($this->number_image) : null,
-                'title' => $this->filial_description,
                 'id' => $this->id,
             ],
             [
                 'value' => $this->mail,
                 'image' => $this->mail_image ? asset($this->mail_image) : null,
-                'title' => $this->filial_description,
                 'id' => $this->id + 1,
             ],
             [
                 'value' => $this->address,
                 'image' => $this->address_image ? asset($this->address_image) : null,
-                'title' => $this->filial_description,
                 'id' => $this->id + 2,
             ]
         ];
